177f866e7217249016adddb6cf1472e96aab1a59
ActorExpose <57215674+ActorExpose@users.noreply.github.com>
2021-01-25 01:57:59 +0900

    Add files via upload

---
+++ aluminum pinger.bat
@@ -0,0 +1,69 @@
+title aluminum ip pinger - Made by renzoslams
+echo off & cls
+color E
+mode con lines=20 cols=70
+echo welcome to aluminum pinger (slam this skid)  
+echo if u wanna cop aluminum hit up renzo.slams on insta 
+set /p IP=Enter Skid IP:
+echo.
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 01
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMCAKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo Skid SMACKED %IP% WHAT A PUSSY...)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
